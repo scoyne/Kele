b81833fbc2d6bf0bb7ee520b9405c2c8ddc9ea8b
scoyne <scoyne@bw.edu>
2018-06-25 23:02:35 -0400

    initialize and authorize kele

--- lib/kele.rb
+++ lib/kele.rb
@@ -6,8 +6,10 @@ class Kele
 
     # initializing kele class by creating a new Kele client authorized with a username and password
     def initialize(email, password)
-        response = self.class.post 'https://www.bloc.io/api/v1/sessions', body { email: email, password: password }
-        puts response
-        @auth_token = "eyJ0eXAiOiJKV1QiLCJhhGciOiJIUzI1NiJ9.eyJhcGlfa2V5IjoiYTc2MDZkNTBhYjA3NDE4ZWE4ZmU5NzliY2YxNTM1ZjAiLCJ1c2VyX2lkIjoyMzAzMTExLCJuYW1lIjoiQmVuIE5lZWx5In0.3VXD-FxOoxaGXHu6vmL8g191bl5F_oKe9qj8Khmp9F0" # Used to obtain auth token required to make API requests
+        response = self.class.post('https://www.bloc.io/api/v1/sessions', body: { email: email, password: password })
+        @auth_token = response["auth_token"] # Used to obtain auth token required to make API requests
+        raise "Invalid email address or password!" if response.code != 200
+        puts response.email
     end
+    
 end
