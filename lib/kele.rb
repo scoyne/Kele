c60e1c035585ec7a3f43ead3fe98f034dc89c518
scoyne <scoyne@bw.edu>
2018-06-26 22:10:38 -0400

    retrieve user record with JSON blob

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
     include HTTParty
@@ -8,8 +9,12 @@ class Kele
     def initialize(email, password)
         response = self.class.post('https://www.bloc.io/api/v1/sessions', body: { email: email, password: password })
         @auth_token = response["auth_token"] # Used to obtain auth token required to make API requests
-        raise "Invalid email address or password!" if response.code != 200
-        puts response.email
+        raise "Invalid email address or password!" if response.code == 404
     end
-    
+
+    # retrieving the current user
+    def get_me
+        response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token })
+        JSON.parse(response.body)
+    end
 end
